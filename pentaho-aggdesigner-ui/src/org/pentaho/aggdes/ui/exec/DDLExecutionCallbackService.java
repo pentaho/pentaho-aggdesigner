25523a66411202ae68f3bead3fc24bbd632f34d5
Ezequiel Cuellar-Ojeda <ecuellar@pentaho.com>
2008-10-21 19:23:27 +0000

    Provided mechanism for callback calls between aggdesigner UI and plugins.
    Enchancement needed for Last Executed Date feature
    
    http://jira.pentaho.com/browse/SURFBOARD-489

---
+++ pentaho-aggdesigner-ui/src/org/pentaho/aggdes/ui/exec/DDLExecutionCallbackService.java
@@ -0,0 +1,36 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License, version 2 as published by the Free Software 
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this 
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html 
+ * or from the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2008 Pentaho Corporation.  All rights reserved. 
+*/
+package org.pentaho.aggdes.ui.exec;
+
+import java.util.List;
+
+/**
+ * Provides a service to install DDLExecutionCompleteCallback implementations. 
+ * These are callback mechanisms between pentaho-aggdesigner-ui and plugins.
+ */
+public class DDLExecutionCallbackService {
+    private List<DDLExecutionCompleteCallback> ddlCallbacks;
+
+    public void setDdlCallbacks(List<DDLExecutionCompleteCallback> ddlCallbacks) {
+        this.ddlCallbacks = ddlCallbacks;
+    }
+    
+    public List<DDLExecutionCompleteCallback> getDdlCallbacks() {
+        return ddlCallbacks;
+    }
+}
