ecceccbfa11190bfb10875a13fad900d749f4dd1
Hilario Fernandes <hfernandes@pentaho.com>
2017-09-27 15:58:30 +0100

    [SP-3993][BACKLOG-17518] Removing transitive Kettle deps of dbdialog and bringing assembly module structure closer to standards

---
+++ assemblies/pad-ce/src/main/resources-filtered/startaggregationdesigner.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# ${project.name}
+# ${project.version}
+# Copyright © 2013 ${project.organization.name}
+# Classpath is built by launcher. See ../lib/launcher.properties.
+
+DIR_REL=`dirname $0`
+cd $DIR_REL
+DIR=`pwd`
+cd -
+
+. "$DIR/set-pentaho-env.sh"
+setPentahoEnv
+
+"$_PENTAHO_JAVA" -jar "$DIR/lib/pentaho-application-launcher-${dependency.pentaho-launcher.revision}.jar"
