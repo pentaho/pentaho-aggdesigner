207100c87bc1c9d300e2677e708c1a6d790c9c45
Bruno Antonio <37145358+bantonio82@users.noreply.github.com>
2019-08-26 10:35:13 +0100

    [SP-5236] - Backport of PAD-147 - Cannot connect to Mondrian with SQL… (#117)
    * [CLEANUP] fixes checkstyle issues
    * [SP-5236] - Backport of PAD-147 - Cannot connect to Mondrian with SQL Server datasource using Integrated Security (7.1 Suite)

--- pentaho-aggdesigner-core/src/test/java/org/pentaho/aggdes/test/AllTests.java
+++ pentaho-aggdesigner-core/src/test/java/org/pentaho/aggdes/test/AllTests.java
@@ -1,41 +1,42 @@
 /*
-* This program is free software; you can redistribute it and/or modify it under the
-* terms of the GNU General Public License, version 2 as published by the Free Software
-* Foundation.
-*
-* You should have received a copy of the GNU General Public License along with this
-* program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
-* or from the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See the GNU General Public License for more details.
-*
-*
-* Copyright 2006 - 2016 Pentaho Corporation.  All rights reserved.
-*/
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License, version 2 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/gpl-2.0.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ *
+ * Copyright 2006 - 2019 Hitachi Vantara.  All rights reserved.
+ */
 
 package org.pentaho.aggdes.test;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import org.pentaho.aggdes.model.mondrian.ValidationMondrianSchemaLoaderTest;
 
 public class AllTests {
 
   public static Test suite() {
-    TestSuite suite = new TestSuite("Test for org.pentaho.aggdes.test");
+    TestSuite suite = new TestSuite( "Test for org.pentaho.aggdes.test" );
     //$JUnit-BEGIN$
-    suite.addTestSuite(MondrianSchemaLoaderTestIT.class);
-    suite.addTestSuite(AlgorithmImplTest.class);
-    suite.addTestSuite(LatticeImplTest.class);
-    suite.addTestSuite(OutputServiceImplTest.class);
-    suite.addTestSuite(MonteCarloLatticeImplTest.class);
-    suite.addTestSuite(AggDesignerMainTest.class);
-    suite.addTestSuite(MondrianSchemaOutputTestIT.class);
-    suite.addTestSuite(TableGeneratorTestIT.class);
-    suite.addTestSuite(SsasToMondrianTest.class);
-    suite.addTestSuite(ValidationMondrianSchemaLoaderTest.class);
+    suite.addTestSuite( MondrianSchemaLoaderTestIT.class );
+    suite.addTestSuite( AlgorithmImplTest.class );
+    suite.addTestSuite( LatticeImplTest.class );
+    suite.addTestSuite( OutputServiceImplTest.class );
+    suite.addTestSuite( MonteCarloLatticeImplTest.class );
+    suite.addTestSuite( AggDesignerMainTest.class );
+    suite.addTestSuite( MondrianSchemaOutputTestIT.class );
+    suite.addTestSuite( TableGeneratorTestIT.class );
+    suite.addTestSuite( SsasToMondrianTest.class );
+    suite.addTestSuite( ValidationMondrianSchemaLoaderTest.class );
     //$JUnit-END$
     return suite;
   }
